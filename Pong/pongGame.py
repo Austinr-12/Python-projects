5c1b42225a9fa0023840c39900f814aec3274097
Austin-12 <austin.ramirez1@outlook.com>
2026-02-15 14:47:13 -0600

    making the players and the ball for setup

--- Pong/pongGame.py
+++ Pong/pongGame.py
@@ -9,9 +9,33 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 def main():
   # GAME SETUP
+  """
+  these are the players' game paddles
+  the pygame.Rect function need the x, y, width and height
+  of the rectangles we will be drawing
+  """
+  paddle_1_rect = pygame.Rect(30, 0, 7, 100)
+  paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
 
-  # initialize the PyGame library (this is absolutely necessary)
-  pygame.init()
+  # this is to track by how much the players' paddles will move per frame
+  paddle_1_move = 0
+  paddle_2_move = 0
+
+  # this is the rectangle that represents the ball
+  ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+
+  # determine the x and y speed of the ball (0.1 is just to scale the speed down)
+  ball_accel_x = random.randint(2, 4) * 0.1
+  ball_accel_y = random.randint(2, 4) * 0.1
+
+  # randomize the direction of the ball
+  if random.randint(1, 2) == 1:
+    ball_accel_x *= -1
+  if random.randint(1, 2) == 1:
+    ball_accel_y *= -1
+
+    # initialize the PyGame library (this is absolutely necessary)
+    pygame.init()
 
   # this creates the window for the game
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
